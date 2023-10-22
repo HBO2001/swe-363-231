f0267ff0806e763cfb7f76f3e93da02857265cfa
HBO2001 <143583650+HBO2001@users.noreply.github.com>
2023-10-22 20:50:45 +0300

    JS04_Exercise02

---
+++ Keyboard_Msg.js
@@ -0,0 +1,6 @@
+(function (){
+
+    alert("How to nativate this website using only a keyboard?\n\nYou can select each element of the website by pressing the (Tab) key or by using the arrow keys.\n\nPressing (Tab + Shift) will go backwards in the selection. \n\nAfter selecting the element. Press the (Enter) key.");
+
+})()
+
