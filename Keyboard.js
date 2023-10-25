3dc593fc89dbf0d6413fe964d8b5642004bc02b5
HBO2001 <143583650+HBO2001@users.noreply.github.com>
2023-10-25 09:06:22 +0300

    JS02_Exercise02

--- Keyboard.js
+++ Keyboard.js
@@ -1,32 +1,87 @@
 (function (){
 
-    alert("How to nativate this website using only a keyboard?\n\nYou can select each element of the website by pressing the (Tab) key or by using the arrow keys.\n\nPressing (Tab + Shift) will go backwards in the selection. \n\nAfter selecting the element. Press the (Enter) key.");
+    alert("How to nativate this website using only a keyboard?\n\nYou can select each element of the website by\n1- pressing the (Tab) key\n2- by using the left and right arrow keys.\n3-Pressing (Tab + Shift) to go backwards in the selection. \n\nClicking a Selected Element: \nPress the (Enter) key.");
 
 })()
 
+                Elements = document.querySelectorAll("a", "button");
 
-window.addEventListener('keydown', (event) => {
+                var Length = Elements.length;
 
-    switch (event.key) {
+                console.log(Elements);
 
-        case 'ArrowLeft':
-        case 'a':
-            {
-                event.preventDefault();
-                console.log('Left')
-                break;
+                var Index = 0;
 
-            }
+                Elements[0].classList.add("selected");
 
-        case 'ArrowRight':
-        case 'd':
-            {
-                event.preventDefault();
-                console.log('Right')
-                break;
+                document.addEventListener('keydown', (event) => {
 
-            }
-            
-    }
-    });
+                switch (event.key) {
 
+
+                    case 'ArrowLeft':
+                    
+                        {
+                            event.preventDefault();
+
+                            Elements[Index].classList.remove("selected");
+
+                            if(Index == 0){
+
+                                Index = Length-1;
+
+                            }
+
+                            else{
+                                
+                                Index = Index - 1;
+
+                            }
+
+                            Elements[Index].classList.add("selected");
+
+                            console.log('Left')
+
+                            break;
+
+                        }
+
+                    case 'ArrowRight':
+                    
+                        {
+                            event.preventDefault();
+                        
+                            Elements[Index].classList.remove("selected");
+
+                            if(Index == Length-1){
+
+                                Index = 0;
+
+                            }
+
+                            else{
+
+                                Index = Index + 1;
+
+                            }
+
+                            Elements[Index].classList.add("selected");
+
+                            console.log('Right')
+
+                            break;
+
+                        }
+
+                    case 'Enter':
+                        {
+
+                            console.log('Enter');
+
+                            Elements[Index].click();
+                        
+
+                        }
+                        
+                   }
+                    });
